a923e56843b07b16e670982d2b63bbaa3d63ae50
Josh <josh.gumerove@gmail.com>
2022-03-15 02:35:55 -0400

    multiplication table

---
+++ MultiplicationTableRunner.java
@@ -0,0 +1,7 @@
+public class MultiplicationTableRunner {
+
+    public static void main(String[] args){
+        MultiplicationTable table = new MultiplicationTable();
+        table.print();
+    }
+}
